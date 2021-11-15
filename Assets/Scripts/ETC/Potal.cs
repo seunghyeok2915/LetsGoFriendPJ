b273038e2a13993eca25f04d2a616ebb242da656
승핵 <sping__@naver.com>
2021-11-15 20:35:42 +0900

    미니맵 하고 포탈 수정

--- Assets/Scripts/ETC/Potal.cs
+++ Assets/Scripts/ETC/Potal.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 public class Potal : MonoBehaviour
 {
     public UnityAction onEnter;
+    private bool usedPotal = false;
 
     public void SetEvent(UnityAction callback)
     {
@@ -12,8 +13,9 @@ public class Potal : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !usedPotal)
         {
+            usedPotal = true;
             onEnter?.Invoke();
         }
     }
